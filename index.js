ffe79b1ac0bea0822f00cc648554b92d02e0dcc9
vincula1 <141301612+vincula1@users.noreply.github.com>
2023-11-28 07:56:54 -0600

    Added seperate route files for each routing and added more to seed file

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-const mongoose = require('mongoose');
-const express = require('express');
-const userRoutes = require('./routes'); // require routes.js
+    const mongoose = require('mongoose');
+    const express = require('express');
+    const userRoutes = require('./routes'); // require routes.js
 
-require('dotenv').config();
+    require('dotenv').config();
 
-const app = express();
+    const app = express();
 
-app.use(express.json()); // parse JSON req bodies
+    app.use(express.json()); // parse JSON req bodies
 
-app.use('/api/users', userRoutes);
+    app.use('/api/users', userRoutes);
 
-mongoose.connect(process.env.MONGODB_URI, { 
-    useNewUrlParser: true, 
-    useUnifiedTopology: true 
-})
-.then(() => console.log('Connected to MongoDB'))
-.catch(err => console.error('Could not connect to MongoDB', err));
+    mongoose.connect(process.env.MONGODB_URI, { 
+        useNewUrlParser: true, 
+        useUnifiedTopology: true 
+    })
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.error('Could not connect to MongoDB', err));
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
--- seed.js
+++ seed.js
@@ -10,12 +10,16 @@ mongoose.connect('mongodb+srv://root:root123@socialnet.jynlqut.mongodb.net/?retr
 
 const users = [
     { username: 'alice', email: 'alice@example.com' },
-    { username: 'bob', email: 'bob@example.com' }
+    { username: 'bob', email: 'bob@example.com' },
+    { username: 'charlie', email: 'charlie@example.com' },
+    { username: 'dave', email: 'dave@example.com' },
 ];
 
 const thoughts = [
-    { thoughtText: "I love coding!", username: "alice" },
-    { thoughtText: "Mongoose makes MongoDB easy.", username: "bob" }
+    { thoughtText: "I love polar bears!", username: "alice" },
+    { thoughtText: "Leah is a good instructor", username: "bob" },
+    { thoughtText: "WHAT IS GOING ON??", username: "charlie" },
+    { thoughtText: "Yippie!!!", username: "dave" },
 ];
 
 const seedDB = async () => {
@@ -23,18 +27,34 @@ const seedDB = async () => {
     await User.deleteMany({});
     await Thought.deleteMany({});
 
-    // Add new data
+    // Adding user data
     const createdUsers = await User.insertMany(users);
 
-    const createdThoughts = thoughts.map(thought => {
+    // Add new thought data with user references
+    const createdThoughts = await Promise.all(thoughts.map(async thought => {
         const user = createdUsers.find(user => user.username === thought.username);
         if (user) {
-            thought.username = user._id;
+            return await Thought.create({ ...thought, username: user._id });
         }
-        return thought;
-    });
+    }));
 
-    await Thought.insertMany(createdThoughts);
+    // Add friends to users
+    await User.findByIdAndUpdate(createdUsers[0]._id, { $addToSet: { friends: createdUsers[1]._id } });
+    await User.findByIdAndUpdate(createdUsers[1]._id, { $addToSet: { friends: createdUsers[0]._id } });
+    await User.findByIdAndUpdate(createdUsers[2]._id, { $addToSet: { friends: createdUsers[3]._id } });
+    await User.findByIdAndUpdate(createdUsers[3]._id, { $addToSet: { friends: createdUsers[2]._id } });
+
+    // Add reactions to thoughts
+    const reaction1 = {
+        reactionBody: "That's so true!",
+        username: createdUsers[1]._id // Bob's reaction to Alice's thought
+    };
+    const reaction2 = {
+        reactionBody: "Absolutely agree!",
+        username: createdUsers[3]._id // Dave's reaction to Charlie's thought
+    };
+    await Thought.findByIdAndUpdate(createdThoughts[0]._id, { $push: { reactions: reaction1 } });
+    await Thought.findByIdAndUpdate(createdThoughts[2]._id, { $push: { reactions: reaction2 } });
 
     console.log('Database seeded!');
 };
